74bfc152aeb11ee6f1ba2c6dfbbe2d8c5ee8f8bf
Guyo <54337290+cipher241@users.noreply.github.com>
2025-11-06 07:39:52 -0600

    Use connection string for database connection
    Refactor database connection handling to use a connection string from environment variables.

--- api/index.py
+++ api/index.py
@@ -7,13 +7,12 @@ import os
 load_dotenv()
 
 # Fetch variables
-USER = os.getenv("user")
-PASSWORD = os.getenv("password")
-HOST = os.getenv("host")
-PORT = os.getenv("port")
-DBNAME = os.getenv("dbname")
+CONNECTION_STRING = os.getenv("CONN_STRING")
     
 app = Flask(__name__)
+
+def get_connection():
+    return psycopg2.connect(CONNECTION_STRING)
 
 @app.route('/')
 def home():
